d2e654f7260246619eec01d4d875f9215d837e84
Eduvieira-1 <edu.vieira1@icloud.com>
2023-11-15 17:21:51 -0300

    property binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  public nome: string = "Dener";
+  public idade: number = 29;
+
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2010/10/07/500x_keizersgrachtreguliersgrachtamsterdamcompressed.jpg";
+  public imgTitle: string = "Property Binding";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
